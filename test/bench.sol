b8b1cef7d27485e041cab51ee16bb7af50122e53
Arto Bendiken <arto@aurora.dev>
2022-09-13 13:36:45 +0400

    Import a benchmark Solidity test contract.
    Co-authored-by: Michael Birch <michael.birch@aurora.dev>

---
+++ test/bench.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.16;
+
+contract Bench {
+    function cpu_ram_soak_test(uint32 loop_limit) public pure {
+        uint8[102400] memory buf;
+        uint32 len = 102400;
+        for (uint32 i=0; i < loop_limit; i++) {
+            uint32 j = (i * 7 + len / 2) % len;
+            uint32 k = (i * 3) % len;
+            uint8 tmp = buf[k];
+            buf[k] = buf[j];
+            buf[j] = tmp;
+        }
+    }
+}
