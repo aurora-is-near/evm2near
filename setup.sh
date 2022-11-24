d8b8458a904ca5400ae40e772fceaa925bd0adb9
Viktar Makouski <mcviktor@tut.by>
2022-11-24 19:03:00 +0400

    CI

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
-curl -sSf https://sh.rustup.rs | sh
-apt-add-repository ppa:ethereum/ethereum
-apt update
-apt upgrade -y
-apt install solc -y
-apt install wabt -y
+sudo curl -sSf https://sh.rustup.rs | sh
+sudo apt-add-repository ppa:ethereum/ethereum
+sudo apt update
+sudo apt upgrade -y
+sudo apt install solc -y
+sudo apt install wabt -y
