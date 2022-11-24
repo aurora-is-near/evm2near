beaedb5e1c7b25286923a7aeb56e91eeecf20d24
Viktar Makouski <mcviktor@tut.by>
2022-11-24 23:37:08 +0400

    rm tmp file

--- test.py
+++ test.py
@@ -1,22 +1,25 @@
 from os import system
 import json
+import subprocess
 
 
 def inputize(d : dict) -> str:
     return '\'' + json.dumps(d) + '\''
 
 
-tmpfile = 'tmp0801371794747427242472.txt'
 deployed = {}
 
 
 def deploy(file : str):
     system(f'./evm2near {file} -o test.wasm -b near')
     print(f'Contract {file} compiled succesfully')
-    system(f'near --networkId testnet dev-deploy test.wasm >{tmpfile}')
-    with open(tmpfile, 'r') as f:
-        s = f.readline()
-        accountId = s[s.find('dev'):s.find(',')]
+    out = subprocess.run(["near",
+                                "--networkId",
+                                "testnet",
+                                "dev-deploy",
+                                "test.wasm"],
+                               capture_output=True).stdout.decode('ascii')
+    accountId = out[out.find('dev'):out.find(',')]
     print(f'Contract {file} deployed to testnet with accountId={accountId}')
     deployed[file] = accountId
 
@@ -25,11 +28,17 @@ def TestOne(file : str, method : str, input : dict, expected_output : str):
     if file not in deployed:
         deploy(file)
     accountId = deployed[file]
-    system(f'near call --account-id {accountId} {accountId}' + f' {method}  {inputize(input)}' + f' >{tmpfile}')
-    with open(tmpfile, 'r') as f:
-        s = f.readlines()[-1]
-        print(f'Call to {file} with args={method}{inputize(input)} return {s[:-1]}')
-        real_output = s[s.find(': ') + 2:s.find(',')]
+    out = subprocess.run(["near",
+                          "call",
+                          "--account-id",
+                          f"{accountId}",
+                          f"{accountId}",
+                          f"{method}",
+                          f"{inputize(input)}"],
+                         capture_output=True).stdout.decode('ascii')[:-1]     # rm \n in the end
+    lastline = out[out.rfind('\n'):]
+    print(f'Call to {file} with args={method}{inputize(input)} return {lastline}')
+    real_output = lastline[lastline.find(': ') + 2:lastline.find(',')]
     assert real_output == expected_output
 
 
@@ -43,9 +52,10 @@ def All():
 
 
 def main():
-    system(f'touch {tmpfile}')
     system('make')
     All()
-    system(f'rm {tmpfile}')
+    system('rm test.wasm')
 
-main()
+
+if __name__ == '__main__':
+    main()
