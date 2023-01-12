a8a41040e22a2eddebaf302da7babe4377594dea
Viktar <mcviktor@tut.by>
2023-01-12 10:08:38 +0000

    review-fixes-and-cleanup

--- lib/relooper/src/graph/reducability.rs
+++ lib/relooper/src/graph/reducability.rs
@@ -1,6 +1,6 @@
 use crate::graph::cfg::CfgEdge;
 use crate::graph::EnrichedCfg;
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::{HashMap, HashSet};
 
 impl crate::CfgLabel for usize {}
 
@@ -27,15 +27,15 @@ pub type Color = usize;
 ///      for actual details of order implementation.
 pub struct ColoredCfg {
     cfg: Cfg,
-    colors: BTreeMap<CfgLabel, Color>,
+    colors: HashMap<CfgLabel, Color>,
     next_cfg_id: CfgLabel,
-    clone2origin: BTreeMap<CfgLabel, CfgLabel>,
+    clone2origin: HashMap<CfgLabel, CfgLabel>,
 }
 
 impl ColoredCfg {
     /// Constructor from Cfg
     pub fn new(cfg: &Cfg) -> ColoredCfg {
-        let mut colors: BTreeMap<CfgLabel, Color> = BTreeMap::default();
+        let mut colors: HashMap<CfgLabel, Color> = HashMap::default();
         let mut id: CfgLabel = 0;
         for (lbl, _edge) in &cfg.out_edges {
             colors.insert(*lbl, *lbl);
@@ -48,7 +48,7 @@ impl ColoredCfg {
             cfg: cfg.clone(),
             colors: colors,
             next_cfg_id: id,
-            clone2origin: BTreeMap::default(),
+            clone2origin: HashMap::default(),
         };
     }
 
@@ -77,12 +77,6 @@ impl ColoredCfg {
             }
             break;
         }
-        // Just debug assertation to check algorithms invariant. Can be removed.
-        let mut different_colors: BTreeSet<Color> = BTreeSet::default();
-        for (_label, color) in &self.colors {
-            different_colors.insert(*color);
-        }
-        assert_eq!(different_colors.len(), 1);
     }
 
     /// This function merges two colors. It simply recolor all nodes with color = slave to master color.
@@ -103,30 +97,22 @@ impl ColoredCfg {
     ///
     /// And a nice trick. If we do it for all masters we will need to delete original slaves. Better is just skip
     /// this operation for one master. This master will have outedges to original slaves (and only this master).
-    pub fn split(&mut self, mut masters: BTreeSet<Color>, slave: Color) -> () {
-        // first delete one random master
+    pub fn split(&mut self, mut masters: HashSet<Color>, slave: Color) -> () {
         let random = masters.iter().next().unwrap().clone();
         masters.remove(&random);
-        // then consequently make a copy of slave for each master color
         for master in &masters {
-            // find all nodes with color = slave
-            let mut slaves: BTreeSet<CfgLabel> = BTreeSet::default();
-            for (label, color) in &self.colors {
-                if *color == slave {
-                    slaves.insert(*label);
-                }
-            }
-            // find all nodes with this master color
-            let mut masternodes: BTreeSet<CfgLabel> = BTreeSet::default();
-            for (label, color) in &self.colors {
-                if *color == *master {
-                    masternodes.insert(*label);
-                }
-            }
-            // make a copy of all nodes with color = slave for this master
-            // with all outedges
-            let mut origin2clone: BTreeMap<CfgLabel, CfgLabel> = BTreeMap::default();
-            let mut clones: BTreeSet<CfgLabel> = BTreeSet::default();
+            let mut slaves: HashSet<CfgLabel> = self
+                .colors
+                .iter()
+                .filter_map(|(label, color)| if color == &slave { Some(*label) } else { None })
+                .collect();
+            let mut masternodes: HashSet<CfgLabel> = self
+                .colors
+                .iter()
+                .filter_map(|(label, color)| if color == master { Some(*label) } else { None })
+                .collect();
+            let mut origin2clone: HashMap<CfgLabel, CfgLabel> = HashMap::default();
+            let mut clones: HashSet<CfgLabel> = HashSet::default();
             for slave_node in &slaves {
                 let copy_label = self.next_cfg_id;
                 self.next_cfg_id += 1;
@@ -137,7 +123,6 @@ impl ColoredCfg {
                 let edge = self.cfg.out_edges.get(&slave_node).unwrap().clone();
                 self.cfg.out_edges.insert(copy_label, edge);
             }
-            // fix edges between slave nodes
             for node in &clones {
                 let edge = self.cfg.out_edges.get_mut(&node).unwrap();
                 match edge {
@@ -165,7 +150,6 @@ impl ColoredCfg {
                     CfgEdge::Terminal => {}
                 }
             }
-            // switch direction of inedges from this master to copyes
             for node in masternodes {
                 let edge = self.cfg.out_edges.get_mut(&node).unwrap();
                 match edge {
@@ -196,10 +180,8 @@ impl ColoredCfg {
         }
     }
 
-    /// This function returns pair of colors (master, slave) if all precessors of all nodes with color = slave
-    /// have color = slave or color = master. If there is no such nodes function returns None.
-    pub fn mergeble_colors(&self) -> Option<(Color, Color)> {
-        let mut precs: BTreeMap<Color, BTreeSet<Color>> = BTreeMap::default();
+    pub fn count_precessors(&self) -> HashMap<Color, HashSet<Color>> {
+        let mut precs: HashMap<Color, HashSet<Color>> = HashMap::default();
         for (node, edge) in &self.cfg.out_edges {
             match edge {
                 CfgEdge::Cond(cond, uncond) => {
@@ -221,194 +203,185 @@ impl ColoredCfg {
                 CfgEdge::Terminal => {}
             }
         }
+        precs
+    }
+
+    /// This function returns pair of colors (master, slave) if all precessors of all nodes with color = slave
+    /// have color = slave or color = master. If there is no such nodes function returns None.
+    pub fn mergeble_colors(&self) -> Option<(Color, Color)> {
+        let mut precs = self.count_precessors();
         for (color, mut precolors) in precs {
             precolors.remove(&color);
             if precolors.len() == 1 {
                 return Some((precolors.into_iter().next().unwrap(), color));
             }
         }
-        return None;
+        None
     }
 
     /// This function returns group of colors (masters, slave) if all precessors of all nodes with color = slave
     /// have color = slave or masters.contain(color). If there is no such nodes it return None.
-    pub fn splittable_colors(&self) -> Option<(BTreeSet<Color>, Color)> {
-        let mut precs: BTreeMap<Color, BTreeSet<Color>> = BTreeMap::default();
-        for (node, edge) in &self.cfg.out_edges {
-            match edge {
-                CfgEdge::Cond(cond, uncond) => {
-                    precs
-                        .entry(*self.colors.get(cond).unwrap())
-                        .or_default()
-                        .insert(*self.colors.get(node).unwrap());
-                    precs
-                        .entry(*self.colors.get(uncond).unwrap())
-                        .or_default()
-                        .insert(*self.colors.get(node).unwrap());
-                }
-                CfgEdge::Uncond(uncond) => {
-                    precs
-                        .entry(*self.colors.get(uncond).unwrap())
-                        .or_default()
-                        .insert(*self.colors.get(node).unwrap());
-                }
-                CfgEdge::Terminal => {}
-            }
-        }
+    pub fn splittable_colors(&self) -> Option<(HashSet<Color>, Color)> {
+        let mut precs = self.count_precessors();
         for (color, mut precolors) in precs {
             precolors.remove(&color);
             if precolors.len() > 1 {
                 return Some((precolors, color));
             }
         }
-        return None;
+        None
     }
 }
 
-#[test]
-pub fn test_create() {
-    let graph = Cfg::from_edges(
-        vec![
-            (0, CfgEdge::Cond(1, 2)),
-            (1, CfgEdge::Cond(3, 5)),
-            (2, CfgEdge::Uncond(3)),
-            (3, CfgEdge::Uncond(4)),
-            (5, CfgEdge::Cond(6, 7)),
-            (6, CfgEdge::Uncond(8)),
-            (7, CfgEdge::Uncond(8)),
-            (4, CfgEdge::Uncond(9)),
-            (8, CfgEdge::Cond(9, 5)),
-        ],
-        0,
-    )
-    .unwrap();
-    let cgraph = ColoredCfg::new(&graph);
-    let graph2 = cgraph.as_cfg();
-    assert_eq!(graph.out_edges, graph2.out_edges);
-    assert_eq!(graph.entry, graph2.entry);
-}
+#[cfg(test)]
+mod reducing_tests {
+    use super::*;
 
-#[test]
-pub fn test_merge() {
-    let graph = Cfg::from_edges(
-        vec![
-            (0, CfgEdge::Cond(1, 2)),
-            (1, CfgEdge::Cond(3, 5)),
-            (2, CfgEdge::Uncond(3)),
-            (3, CfgEdge::Uncond(4)),
-            (5, CfgEdge::Cond(6, 7)),
-            (6, CfgEdge::Uncond(8)),
-            (7, CfgEdge::Uncond(8)),
-            (4, CfgEdge::Uncond(9)),
-            (8, CfgEdge::Cond(9, 5)),
-        ],
-        0,
-    )
-    .unwrap();
-    let mut cgraph = ColoredCfg::new(&graph);
-    cgraph.merge(6, 7);
-    assert_eq!(*cgraph.colors.get(&7).unwrap(), 6);
-    cgraph.merge(1, 2);
-    assert_eq!(*cgraph.colors.get(&2).unwrap(), 1);
-}
-
-#[test]
-pub fn test_reducable() {
-    let graph = Cfg::from_edges(
-        vec![
-            (0, CfgEdge::Cond(1, 2)),
-            (1, CfgEdge::Cond(3, 5)),
-            (2, CfgEdge::Uncond(3)),
-            (3, CfgEdge::Uncond(4)),
-            (5, CfgEdge::Cond(6, 7)),
-            (6, CfgEdge::Uncond(8)),
-            (7, CfgEdge::Uncond(8)),
-            (4, CfgEdge::Uncond(9)),
-            (8, CfgEdge::Cond(9, 5)),
-        ],
-        0,
-    )
-    .unwrap();
-    let mut cgraph = ColoredCfg::new(&graph);
-    cgraph.reduce_colors();
-    let mut different_colors: BTreeSet<Color> = BTreeSet::default();
-    for (_label, color) in cgraph.colors {
-        different_colors.insert(color);
+    #[test]
+    pub fn test_create() {
+        let graph = Cfg::from_edges(
+            vec![
+                (0, CfgEdge::Cond(1, 2)),
+                (1, CfgEdge::Cond(3, 5)),
+                (2, CfgEdge::Uncond(3)),
+                (3, CfgEdge::Uncond(4)),
+                (5, CfgEdge::Cond(6, 7)),
+                (6, CfgEdge::Uncond(8)),
+                (7, CfgEdge::Uncond(8)),
+                (4, CfgEdge::Uncond(9)),
+                (8, CfgEdge::Cond(9, 5)),
+            ],
+            0,
+        )
+        .unwrap();
+        let cgraph = ColoredCfg::new(&graph);
+        let graph2 = cgraph.as_cfg();
+        assert_eq!(graph.out_edges, graph2.out_edges);
+        assert_eq!(graph.entry, graph2.entry);
     }
-    assert_eq!(different_colors.len(), 1);
-}
 
-#[test]
-pub fn test_irreducable() {
-    let graph = Cfg::from_edges(
-        vec![
-            (0, CfgEdge::Cond(1, 2)),
-            (1, CfgEdge::Cond(2, 3)),
-            (2, CfgEdge::Uncond(1)),
-        ],
-        0,
-    )
-    .unwrap();
-    let mut cgraph = ColoredCfg::new(&graph);
-    cgraph.reduce_colors();
-    let reduced = cgraph.as_cfg();
-    let e_graph = EnrichedCfg::new(reduced);
-    let dot_lines: Vec<String> = vec![
-        "digraph {".to_string(),
-        e_graph.cfg_to_dot("reduced"),
-        "}".to_string(),
-    ];
-    std::fs::write("reduced.dot", dot_lines.join("\n")).expect("fs error");
-}
+    #[test]
+    pub fn test_merge() {
+        let graph = Cfg::from_edges(
+            vec![
+                (0, CfgEdge::Cond(1, 2)),
+                (1, CfgEdge::Cond(3, 5)),
+                (2, CfgEdge::Uncond(3)),
+                (3, CfgEdge::Uncond(4)),
+                (5, CfgEdge::Cond(6, 7)),
+                (6, CfgEdge::Uncond(8)),
+                (7, CfgEdge::Uncond(8)),
+                (4, CfgEdge::Uncond(9)),
+                (8, CfgEdge::Cond(9, 5)),
+            ],
+            0,
+        )
+        .unwrap();
+        let mut cgraph = ColoredCfg::new(&graph);
+        cgraph.merge(6, 7);
+        assert_eq!(*cgraph.colors.get(&7).unwrap(), 6);
+        cgraph.merge(1, 2);
+        assert_eq!(*cgraph.colors.get(&2).unwrap(), 1);
+    }
 
-#[test]
-pub fn test_irreducable2() {
-    let graph = Cfg::from_edges(
-        vec![
-            (0, CfgEdge::Uncond(1)),
-            (1, CfgEdge::Cond(2, 3)),
-            (2, CfgEdge::Cond(4, 3)),
-            (3, CfgEdge::Cond(2, 5)),
-            (4, CfgEdge::Cond(6, 5)),
-            (5, CfgEdge::Cond(4, 7)),
-        ],
-        0,
-    )
-    .unwrap();
-    let mut cgraph = ColoredCfg::new(&graph);
-    cgraph.reduce_colors();
-    let reduced = cgraph.as_cfg();
-    let e_graph = EnrichedCfg::new(reduced);
-    let dot_lines: Vec<String> = vec![
-        "digraph {".to_string(),
-        e_graph.cfg_to_dot("reduced"),
-        "}".to_string(),
-    ];
-    std::fs::write("reduced2.dot", dot_lines.join("\n")).expect("fs error");
-}
+    #[test]
+    pub fn test_reducable() {
+        let graph = Cfg::from_edges(
+            vec![
+                (0, CfgEdge::Cond(1, 2)),
+                (1, CfgEdge::Cond(3, 5)),
+                (2, CfgEdge::Uncond(3)),
+                (3, CfgEdge::Uncond(4)),
+                (5, CfgEdge::Cond(6, 7)),
+                (6, CfgEdge::Uncond(8)),
+                (7, CfgEdge::Uncond(8)),
+                (4, CfgEdge::Uncond(9)),
+                (8, CfgEdge::Cond(9, 5)),
+            ],
+            0,
+        )
+        .unwrap();
+        let mut cgraph = ColoredCfg::new(&graph);
+        cgraph.reduce_colors();
+        let mut different_colors: HashSet<Color> = HashSet::default();
+        for (_label, color) in cgraph.colors {
+            different_colors.insert(color);
+        }
+        assert_eq!(different_colors.len(), 1);
+    }
 
-#[test]
-pub fn test_irreducable3() {
-    let graph = Cfg::from_edges(
-        vec![
-            (0, CfgEdge::Cond(1, 2)),
-            (1, CfgEdge::Cond(3, 4)),
-            (2, CfgEdge::Cond(4, 5)),
-            (3, CfgEdge::Cond(4, 6)),
-            (4, CfgEdge::Cond(3, 5)),
-            (5, CfgEdge::Cond(4, 7)),
-        ],
-        0,
-    )
-    .unwrap();
-    let mut cgraph = ColoredCfg::new(&graph);
-    cgraph.reduce_colors();
-    let reduced = cgraph.as_cfg();
-    let e_graph = EnrichedCfg::new(reduced);
-    let dot_lines: Vec<String> = vec![
-        "digraph {".to_string(),
-        e_graph.cfg_to_dot("reduced"),
-        "}".to_string(),
-    ];
-    std::fs::write("reduced3.dot", dot_lines.join("\n")).expect("fs error");
+    #[test]
+    pub fn test_irreducable() {
+        let graph = Cfg::from_edges(
+            vec![
+                (0, CfgEdge::Cond(1, 2)),
+                (1, CfgEdge::Cond(2, 3)),
+                (2, CfgEdge::Uncond(1)),
+            ],
+            0,
+        )
+        .unwrap();
+        let mut cgraph = ColoredCfg::new(&graph);
+        cgraph.reduce_colors();
+        let reduced = cgraph.as_cfg();
+        let e_graph = EnrichedCfg::new(reduced);
+        let dot_lines: Vec<String> = vec![
+            "digraph {".to_string(),
+            e_graph.cfg_to_dot("reduced"),
+            "}".to_string(),
+        ];
+        std::fs::write("reduced.dot", dot_lines.join("\n")).expect("fs error");
+    }
+
+    #[test]
+    pub fn test_irreducable2() {
+        let graph = Cfg::from_edges(
+            vec![
+                (0, CfgEdge::Uncond(1)),
+                (1, CfgEdge::Cond(2, 3)),
+                (2, CfgEdge::Cond(4, 3)),
+                (3, CfgEdge::Cond(2, 5)),
+                (4, CfgEdge::Cond(6, 5)),
+                (5, CfgEdge::Cond(4, 7)),
+            ],
+            0,
+        )
+        .unwrap();
+        let mut cgraph = ColoredCfg::new(&graph);
+        cgraph.reduce_colors();
+        let reduced = cgraph.as_cfg();
+        let e_graph = EnrichedCfg::new(reduced);
+        let dot_lines: Vec<String> = vec![
+            "digraph {".to_string(),
+            e_graph.cfg_to_dot("reduced"),
+            "}".to_string(),
+        ];
+        std::fs::write("reduced2.dot", dot_lines.join("\n")).expect("fs error");
+    }
+
+    #[test]
+    pub fn test_irreducable3() {
+        let graph = Cfg::from_edges(
+            vec![
+                (0, CfgEdge::Cond(1, 2)),
+                (1, CfgEdge::Cond(3, 4)),
+                (2, CfgEdge::Cond(4, 5)),
+                (3, CfgEdge::Cond(4, 6)),
+                (4, CfgEdge::Cond(3, 5)),
+                (5, CfgEdge::Cond(4, 7)),
+            ],
+            0,
+        )
+        .unwrap();
+        let mut cgraph = ColoredCfg::new(&graph);
+        cgraph.reduce_colors();
+        let reduced = cgraph.as_cfg();
+        let e_graph = EnrichedCfg::new(reduced);
+        let dot_lines: Vec<String> = vec![
+            "digraph {".to_string(),
+            e_graph.cfg_to_dot("reduced"),
+            "}".to_string(),
+        ];
+        std::fs::write("reduced3.dot", dot_lines.join("\n")).expect("fs error");
+    }
 }
