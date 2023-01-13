4a846a91dd1e108e026a56e3bc6cf481f158d5ee
Viktar <mcviktor@tut.by>
2023-01-13 09:48:39 +0000

    None

--- lib/relooper/src/graph/reducibility.rs
+++ lib/relooper/src/graph/reducibility.rs
@@ -1,11 +1,14 @@
 use crate::graph::cfg::CfgEdge;
 use crate::graph::EnrichedCfg;
-use std::{collections::{HashMap, HashSet}, fmt::Display};
+use std::{
+    collections::{HashMap, HashSet},
+    fmt::Display,
+};
 
 impl crate::CfgLabel for usize {}
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Hash, Clone, Copy)]
-pub struct PairedLabel(usize, usize);                      // (unique_label, origin_label)
+pub struct PairedLabel(usize, usize); // (unique_label, origin_label)
 impl Display for PairedLabel {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}_{}", self.0, self.1)
@@ -14,7 +17,7 @@ impl Display for PairedLabel {
 
 impl crate::CfgLabel for PairedLabel {}
 type CfgLabel = usize;
-pub type LabeledCfg = crate::Cfg<PairedLabel>; 
+pub type LabeledCfg = crate::Cfg<PairedLabel>;
 type Cfg = crate::Cfg<CfgLabel>;
 pub type Color = usize;
 
@@ -246,26 +249,28 @@ impl ColoredCfg {
         None
     }
 
-    pub fn paired_label(&self, cfg_label : CfgLabel) -> PairedLabel {
+    pub fn paired_label(&self, cfg_label: CfgLabel) -> PairedLabel {
         match self.clone2origin.get(&cfg_label) {
-            Some(origin) => {
-                PairedLabel(*origin, cfg_label)
-            }
-            None => {
-                PairedLabel(cfg_label, cfg_label)
-            }
+            Some(origin) => PairedLabel(*origin, cfg_label),
+            None => PairedLabel(cfg_label, cfg_label),
         }
     }
 
     pub fn as_labeled_cfg(&self) -> LabeledCfg {
-        let mut edges : Vec<(PairedLabel, CfgEdge<PairedLabel>)> = Vec::default();
+        let mut edges: Vec<(PairedLabel, CfgEdge<PairedLabel>)> = Vec::default();
         for (node, edge) in &self.cfg.out_edges {
             match edge {
                 CfgEdge::Cond(cond, uncond) => {
-                    edges.push((self.paired_label(*node), CfgEdge::Cond(self.paired_label(*cond), self.paired_label(*uncond))));
+                    edges.push((
+                        self.paired_label(*node),
+                        CfgEdge::Cond(self.paired_label(*cond), self.paired_label(*uncond)),
+                    ));
                 }
                 CfgEdge::Uncond(uncond) => {
-                    edges.push((self.paired_label(*node), CfgEdge::Uncond(self.paired_label(*uncond))));
+                    edges.push((
+                        self.paired_label(*node),
+                        CfgEdge::Uncond(self.paired_label(*uncond)),
+                    ));
                 }
                 CfgEdge::Terminal => {
                     edges.push((self.paired_label(*node), CfgEdge::<PairedLabel>::Terminal));
@@ -354,7 +359,7 @@ mod reducing_tests {
     }
 
     #[test]
-    pub fn test_irreducible() {
+    pub fn test_as_labeled_cfg() {
         let graph = Cfg::from_edges(
             vec![
                 (0, CfgEdge::Cond(1, 2)),
@@ -366,7 +371,7 @@ mod reducing_tests {
         .unwrap();
         let mut cgraph = ColoredCfg::new(&graph);
         cgraph.reduce_colors();
-        let reduced = cgraph.as_cfg();
+        let reduced = cgraph.as_labeled_cfg();
         let e_graph = EnrichedCfg::new(reduced);
         let dot_lines: Vec<String> = vec![
             "digraph {".to_string(),
@@ -448,7 +453,7 @@ pub fn test_irreducable4() {
     let e_graph = EnrichedCfg::new(reduced);
     let dot_lines: Vec<String> = vec![
         "digraph {".to_string(),
-        e_graph.cfg_to_dot(),
+        e_graph.cfg_to_dot("reduced"),
         "}".to_string(),
     ];
     std::fs::write("reduced4.dot", dot_lines.join("\n")).expect("fs error");
