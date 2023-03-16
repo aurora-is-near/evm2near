7fb8eb54cf00b2268398dfadee77f27261be8233
mcjohn974 <mcvictor@tut.by>
2023-03-16 16:18:18 +0300

    Some more contracts abd naming fix

--- test/bench-mini.sol
+++ test/bench-mini.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.16;
 
 contract Bench {
     function cpu_ram_soak_test(uint32 loop_limit) public pure {
-        uint8[102400] memory buf;
-        uint32 len = 102400;
+        uint8[100] memory buf;
+        uint32 len = 100;
         for (uint32 i=0; i < loop_limit; i++) {
             uint32 j = (i * 7 + len / 2) % len;
             uint32 k = (i * 3) % len;
