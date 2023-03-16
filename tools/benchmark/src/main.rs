e9e47174b9e528b49795c7dffaba57d3bd84f030
mcjohn974 <mcvictor@tut.by>
2023-03-16 17:42:34 +0300

    append should work

--- tools/benchmark/src/main.rs
+++ tools/benchmark/src/main.rs
@@ -1,7 +1,9 @@
 use csv::Writer;
 use serde::{Deserialize};
 use serde_json::{json, Value};
-use std::fs::File;
+use std::{fs::File, ffi::OsString};
+use std::process::Command;
+
 
 #[derive(Debug, Deserialize)]
 struct Input {
@@ -11,7 +13,9 @@ struct Input {
 
 const TERA: u64 = 1000000000000_u64;
 
-async fn bench_contract(wtr: &mut Writer<File>, name: &str) -> anyhow::Result<()> {
+async fn bench_contract(wtr: &mut Writer<File>, name_os: OsString, commit: String) -> anyhow::Result<()> {
+    let name = &name_os.to_str().unwrap()[0..name_os.len() - 5];
+    println!("Name = {}", name);
     let worker = near_workspaces::sandbox().await?;
     let wasm = std::fs::read(format!("{}.wasm", name))?;
     let contract = worker.dev_deploy(&wasm).await?;
@@ -21,13 +25,18 @@ async fn bench_contract(wtr: &mut Writer<File>, name: &str) -> anyhow::Result<()
             .expect("Unable to read file"),
     )
     .expect("JSON does not have correct format.");
-
+    let deposit = 10000000000000000000000_u128;
     for input in &inputs {
         let outcome = contract
             .call(&input.method)
             .args_json(json!(input.input))
+            .deposit(deposit)
+            .gas(near_units::parse_gas!("300000000000000") as u64)
             .transact()
             .await?;
+        for failure in &outcome.failures() {
+            println!("{:#?}", failure);
+        }
         assert!(outcome.is_success());
         wtr.write_record(&[
             name.to_string(),
@@ -37,6 +46,7 @@ async fn bench_contract(wtr: &mut Writer<File>, name: &str) -> anyhow::Result<()
             (outcome.outcome().gas_burnt / TERA).to_string(),
             (outcome.total_gas_burnt / TERA).to_string(),
             input.input.to_string(),
+            commit.clone(),
         ])?;
     }
     wtr.flush()?;
@@ -45,9 +55,26 @@ async fn bench_contract(wtr: &mut Writer<File>, name: &str) -> anyhow::Result<()
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let contracts = vec!["calc"];
 
-    let mut wtr = Writer::from_path("benchmark.csv")?;
+    let paths = std::fs::read_dir("inputs/").unwrap();
+
+    let contracts = paths.into_iter().map(|dir| dir.unwrap().file_name()).collect::<Vec<_>>();
+
+
+
+    let output = Command::new("sh")
+                .arg("-c")
+                .arg("git rev-parse --short HEAD")
+                .output()
+                .expect("failed to execute process");
+    
+    let stdout = output.stdout;
+    let mut commit = std::str::from_utf8(&stdout).unwrap().to_string();
+    commit.pop();  // to remove \n in the end
+
+    
+
+    let mut wtr = Writer::from_path(format!("{}.csv", commit).to_string())?;
     wtr.write_record([
         "Contract",
         "Method",
@@ -56,10 +83,11 @@ async fn main() -> anyhow::Result<()> {
         "Tgas burned",
         "Tgas used",
         "Input",
+        "Commit",
     ])?;
 
     for contract in contracts {
-        bench_contract(&mut wtr, contract).await?;
+        bench_contract(&mut wtr, contract, commit.clone()).await?;
     }
 
     wtr.flush()?;
